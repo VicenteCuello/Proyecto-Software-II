816f4f8aaec0a7ce5c26293eb396e7c621756d32
VicenteCuello <vicuello2021@inf.udec.cl>
2025-05-24 18:55:27 -0400

    boton actividades favoritas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 import CalendarComponent from './components/CalendarComponent';
 import ActivitySelection from './components/ActivitySelection';
 import ManualWeather from './components/ManualWeather';
-import NotificationsPage from './components/Notification'; // ⬅️ Asegúrate de que la ruta sea correcta
-import WeatherPage from './components/WeatherPage'; // <-- IMPORTA el nuevo componente
-import { useState } from 'react';
-import { getWeatherByCity } from './api/weather';
 import Button from '@mui/material/Button';
+
 
 function Main() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      {/* Botón en la esquina superior izquierda */}
+      <div style={{ position: 'absolute', top: '10px', left: '10px' }}>
+        <Link to="/select-activities/favorites">
+          <Button variant="contained" color="primary">
+            Actividades Favoritas
+          </Button>
+        </Link>
+      </div>
+
+      {/* Botón en la esquina superior derecha */}
       <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
         <Link to="/calendar">
           <Button variant="contained" color="primary">
@@ -28,7 +42,15 @@ function Main() {
 
 function CalendarPage() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
         <Link to="/">
           <Button variant="contained" color="primary">
@@ -49,6 +71,7 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/calendar" element={<CalendarPage />} />
           <Route path="/select-activities/:date" element={<ActivitySelection />} />
+          <Route path="/select-activities/favorites" element={<ActivitySelection />} />
         </Routes>
       </Router>
     </ThemeProvider>
@@ -56,6 +79,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
