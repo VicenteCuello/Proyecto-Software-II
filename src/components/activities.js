a41d0c1240887818c0a8bdb40081473597819459
Cote <marcastillo2021@udec.cl>
2025-06-08 22:32:36 -0400

    cambios array activities

--- src/components/activities.js
+++ src/components/activities.js
@@ -3,8 +3,8 @@ export const availableActivities = [
    { name: 'Correr', image: '/images/correr.webp', temperatura: [7, 25], estado: ['soleado', 'nublado', 'viento', 'niebla'] },
    { name: 'Leer', image: '/images/leer.webp', temperatura: [6, 24], estado: ['soleado', 'nublado', 'lluvioso', 'tormenta', 'viento', 'niebla'] },
    { name: 'Estudiar React', image: '/images/estudiar react.webp', temperatura: [5, 24], estado: ['soleado', 'nublado', 'lluvioso', 'tormenta', 'viento', 'niebla'] },
-   { name: 'Ir al cine', image: '/images/ir al cine.webp', temperatura: [18, 22], estado: ['soleado', 'nublado', 'lluvioso', 'viento', 'niebla'] },
-   { name: 'Ir al gym', image: '/images/ir al gym.webp', temperatura: [10, 22], estado: ['soleado', 'nublado', 'lluvioso', 'viento', 'niebla'] },
-   { name: 'Ir de compras', image: '/images/Ir de compras.webp', temperatura: [12, 23], estado: ['soleado', 'nublado', 'lluvioso', 'viento', 'niebla'] },
+   { name: 'Ir al cine', image: '/images/ir al cine.webp', temperatura: [18, 22], estado: ['soleado', 'nublado', 'viento', 'niebla'] },
+   { name: 'Ir al gym', image: '/images/ir al gym.webp', temperatura: [10, 22], estado: ['soleado', 'nublado', 'viento', 'niebla'] },
+   { name: 'Ir de compras', image: '/images/Ir de compras.webp', temperatura: [12, 23], estado: ['soleado', 'nublado', 'viento', 'niebla'] },
    { name: 'Cocinar', image: '/images/cocinar.webp', temperatura: [5, 23], estado: ['soleado', 'nublado', 'lluvioso', 'tormenta', 'viento', 'niebla'] },
 ];
