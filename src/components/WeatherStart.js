279fd97d705454a13384213568e4389674f8ce84
Jogny5 <josejoses1734@gmail.com>
2025-06-08 21:49:21 -0400

    arreglo actividades

--- src/components/WeatherStart.js
+++ src/components/WeatherStart.js
@@ -28,8 +28,8 @@ function WeatherStart() {
   const traducirMainClima = (main) => {
     const traducciones = {
       Thunderstorm: 'Tormenta',
-      Drizzle: 'Lluvia',
-      Rain: 'Lluvia',
+      Drizzle: 'lluvioso',
+      Rain: 'lluvioso',
       Snow: 'Nieve',
       Clear: 'Soleado',
       Clouds: 'Nublado',
