908b21cf70c37a4b7a271ed34af6752de4707548
VicenteCuello <vicuello2021@inf.udec.cl>
2025-06-30 20:52:11 -0400

    correccion para main

--- src/components/CityContext.js
+++ src/components/CityContext.js
@@ -1,14 +1,12 @@
-import React, { createContext, useState, useEffect } from 'react';
-import { getForecastByCity } from '../api/weather';
-import { availableActivities } from './activities';
+import React, { createContext, useState } from 'react';
 
-// 1. Crear el contexto con un valor inicial
+// Crear el contexto con un valor inicial
 export const CityContext = createContext({
   city: 'Concepcion', // Ciudad por defecto
   setCity: () => {},
 });
 
-// 2. Crear un "Proveedor" que envolverá tu aplicación
+// Crear un "Proveedor" que envolverá tu aplicación
 export const CityProvider = ({ children }) => {
   const [city, setCity] = useState('Concepcion'); // El estado real vive aquí
 
